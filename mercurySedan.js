916e1a8faf5e357e61ed3a35c0c8ff26a5aff549
daibourne <daijonbourne1005@gmail.com>
2023-12-21 01:53:15 -0800

    Finish Activity

--- mercurySedan.js
+++ mercurySedan.js
@@ -13,6 +13,32 @@ console.log(v.make)
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
+class Car extends VehicleModule.Vehicle {
+    constructor(maxPassengers, passengers, numberOfWheels, maxSpeed, fuel, scheduleService){
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+    loadPassenger(num) {
+    }
+    start(){
+        if(this.fuel > 0){
+            this.started = true;
+        }
+        else{
+            this.started = false;
+        }
+    }
+    scheduleService(mileage){
+        if(this.mileage > 30000){
+            this.scheduleService = true; 
+        }
+    }
+}
+
 
 
 
